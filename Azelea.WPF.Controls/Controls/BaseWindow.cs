9ae82a9df0d8965688d3f0495796550a3bbf61ff
王慧勇 <debrogile@163.com>
2015-08-14 17:10:17 +0800

    新建项目

---
+++ Azelea.WPF.Controls/Controls/BaseWindow.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Azelea.WPF.Controls
+{
+    [TemplatePart(Name = "PART_Header", Type = typeof(Panel))]
+    public class BaseWindow : Window
+    {
+        static BaseWindow()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(BaseWindow), new FrameworkPropertyMetadata(typeof(BaseWindow)));
+        }
+
+        //private FrameworkElement _Header;
+
+        #region DependencyProperty
+
+        public static readonly DependencyProperty HeaderProperty =
+            DependencyProperty.Register("Header", typeof(Panel), typeof(BaseWindow));
+
+        public Panel Header
+        {
+            get { return (Panel)GetValue(HeaderProperty); }
+            set { SetValue(HeaderProperty, value); }
+        }
+
+        public static readonly DependencyProperty ToolsProperty =
+            DependencyProperty.Register("Tools", typeof(Panel), typeof(BaseWindow));
+
+        public Panel Tools
+        {
+            get { return (Panel)GetValue(ToolsProperty); }
+            set { SetValue(ToolsProperty, value); }
+        }
+
+        #endregion
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+        }
+    }
+}
---
+++ Azelea.WPF.Demo/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Azelea.WPF.Controls;
+
+namespace Azelea.WPF.Demo
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : BaseWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
