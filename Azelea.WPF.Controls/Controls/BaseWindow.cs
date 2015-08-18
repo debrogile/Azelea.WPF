cd1530108e5639ad85ac3f917a23aaa99340dcd1
王慧勇 <debrogile@163.com>
2015-08-18 17:27:10 +0800

    完成窗口大小基本控制功能

--- Azelea.WPF.Controls/Controls/BaseWindow.cs
+++ Azelea.WPF.Controls/Controls/BaseWindow.cs
@@ -1,10 +1,22 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Shapes;
 
 namespace Azelea.WPF.Controls
 {
     [TemplatePart(Name = "PART_Header", Type = typeof(Panel))]
+    [TemplatePart(Name = "PART_Min", Type = typeof(Button))]
+    [TemplatePart(Name = "PART_Restore", Type = typeof(Button))]
+    [TemplatePart(Name = "PART_Close", Type = typeof(Button))]
+    [TemplatePart(Name = "PART_SizeLeft", Type = typeof(Rectangle))]
+    [TemplatePart(Name = "PART_SizeRight", Type = typeof(Rectangle))]
+    [TemplatePart(Name = "PART_SizeTop", Type = typeof(Rectangle))]
+    [TemplatePart(Name = "PART_SizeTopLeft", Type = typeof(Rectangle))]
+    [TemplatePart(Name = "PART_SizeTopRight", Type = typeof(Rectangle))]
+    [TemplatePart(Name = "PART_SizeBottom", Type = typeof(Rectangle))]
+    [TemplatePart(Name = "PART_SizeBottomLeft", Type = typeof(Rectangle))]
+    [TemplatePart(Name = "PART_SizeBottomRight", Type = typeof(Rectangle))]
     public class BaseWindow : Window
     {
         static BaseWindow()
@@ -12,9 +24,35 @@ namespace Azelea.WPF.Controls
             DefaultStyleKeyProperty.OverrideMetadata(typeof(BaseWindow), new FrameworkPropertyMetadata(typeof(BaseWindow)));
         }
 
-        //private FrameworkElement _Header;
+        private FrameworkElement _Header;
+        private Button _Min;
+        private Button _Restore;
+        private Button _Close;
+        private Rectangle _SizeLeft;
+        private Rectangle _SizeRight;
+        private Rectangle _SizeTop;
+        private Rectangle _SizeTopLeft;
+        private Rectangle _SizeTopRight;
+        private Rectangle _SizeBottom;
+        private Rectangle _SizeBottomLeft;
+        private Rectangle _SizeBottomRight;
 
         #region DependencyProperty
+
+        #region CanResize
+
+        public static readonly DependencyProperty CanResizeProperty =
+            DependencyProperty.Register("CanResize", typeof(bool), typeof(BaseWindow), new PropertyMetadata(true));
+
+        public bool CanResize
+        {
+            get { return (bool)GetValue(CanResizeProperty); }
+            set { SetValue(CanResizeProperty, value); }
+        }
+
+        #endregion
+
+        #region Header
 
         public static readonly DependencyProperty HeaderProperty =
             DependencyProperty.Register("Header", typeof(Panel), typeof(BaseWindow));
@@ -24,6 +62,10 @@ namespace Azelea.WPF.Controls
             get { return (Panel)GetValue(HeaderProperty); }
             set { SetValue(HeaderProperty, value); }
         }
+
+        #endregion
+
+        #region Tools
 
         public static readonly DependencyProperty ToolsProperty =
             DependencyProperty.Register("Tools", typeof(Panel), typeof(BaseWindow));
@@ -36,9 +78,120 @@ namespace Azelea.WPF.Controls
 
         #endregion
 
+        #region TryClose
+
+        public static readonly DependencyProperty TryCloseProperty =
+            DependencyProperty.Register("TryClose", typeof(Func<bool>), typeof(BaseWindow),
+                new PropertyMetadata(new Func<bool>(() => true)));
+
+        public Func<bool> TryClose
+        {
+            get { return (Func<bool>)GetValue(TryCloseProperty); }
+            set { SetValue(TryCloseProperty, value); }
+        }
+
+        #endregion
+
+        #endregion
+
+        public BaseWindow()
+        {
+            WindowHelper.RepairWindow(this);
+        }
+
         public override void OnApplyTemplate()
         {
             base.OnApplyTemplate();
+
+            #region WindowState
+
+            _Header = GetTemplateChild("PART_Header") as FrameworkElement;
+            _Header.MouseLeftButtonDown += (s, e) => 
+            {
+                if (e.ClickCount == 1)
+                {
+                    WindowHelper.SendMessage(WindowHelper.Handle, WindowHelper.WM_SYSCOMMAND, (IntPtr)WindowHelper.SC_MOVEHEADER, IntPtr.Zero);
+                }
+                else if (CanResize)
+                {
+                    WindowState = WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+                }
+            };
+
+            _Min = GetTemplateChild("PART_Min") as Button;
+            _Min.Click += delegate 
+            {
+                WindowState = WindowState.Minimized;
+            };
+
+            _Restore = GetTemplateChild("PART_Restore") as Button;
+            _Restore.Click += delegate
+            {
+                WindowState = WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+            };
+
+            _Close = GetTemplateChild("PART_Close") as Button;
+            _Close.Click += delegate 
+            {
+                if (TryClose())
+                {
+                    Close();
+                }
+            };
+
+            #endregion
+
+            #region Window Resize
+
+            _SizeLeft = GetTemplateChild("PART_SizeLeft") as Rectangle;
+            _SizeLeft.MouseLeftButtonDown += delegate
+            {
+                WindowHelper.SendMessage(WindowHelper.Handle, WindowHelper.WM_SYSCOMMAND, (IntPtr)WindowHelper.SC_SIZE_LEFT, IntPtr.Zero);
+            };
+
+            _SizeRight = GetTemplateChild("PART_SizeRight") as Rectangle;
+            _SizeRight.MouseLeftButtonDown += delegate
+            {
+                WindowHelper.SendMessage(WindowHelper.Handle, WindowHelper.WM_SYSCOMMAND, (IntPtr)WindowHelper.SC_SIZE_RIGHT, IntPtr.Zero);
+            };
+
+            _SizeTop = GetTemplateChild("PART_SizeTop") as Rectangle;
+            _SizeTop.MouseLeftButtonDown += delegate
+            {
+                WindowHelper.SendMessage(WindowHelper.Handle, WindowHelper.WM_SYSCOMMAND, (IntPtr)WindowHelper.SC_SIZE_TOP, IntPtr.Zero);
+            };
+
+            _SizeTopLeft = GetTemplateChild("PART_SizeTopLeft") as Rectangle;
+            _SizeTopLeft.MouseLeftButtonDown += delegate
+            {
+                WindowHelper.SendMessage(WindowHelper.Handle, WindowHelper.WM_SYSCOMMAND, (IntPtr)WindowHelper.SC_SIZE_TOPLEFT, IntPtr.Zero);
+            };
+
+            _SizeTopRight = GetTemplateChild("PART_SizeTopRight") as Rectangle;
+            _SizeTopRight.MouseLeftButtonDown += delegate
+            {
+                WindowHelper.SendMessage(WindowHelper.Handle, WindowHelper.WM_SYSCOMMAND, (IntPtr)WindowHelper.SC_SIZE_TOPRIGHT, IntPtr.Zero);
+            };
+
+            _SizeBottom = GetTemplateChild("PART_SizeBottom") as Rectangle;
+            _SizeBottom.MouseLeftButtonDown += delegate
+            {
+                WindowHelper.SendMessage(WindowHelper.Handle, WindowHelper.WM_SYSCOMMAND, (IntPtr)WindowHelper.SC_SIZE_BOTTOM, IntPtr.Zero);
+            };
+
+            _SizeBottomLeft = GetTemplateChild("PART_SizeBottomLeft") as Rectangle;
+            _SizeBottomLeft.MouseLeftButtonDown += delegate
+            {
+                WindowHelper.SendMessage(WindowHelper.Handle, WindowHelper.WM_SYSCOMMAND, (IntPtr)WindowHelper.SC_SIZE_BOTTOMLEFT, IntPtr.Zero);
+            };
+
+            _SizeBottomRight = GetTemplateChild("PART_SizeBottomRight") as Rectangle;
+            _SizeBottomRight.MouseLeftButtonDown += delegate
+            {
+                WindowHelper.SendMessage(WindowHelper.Handle, WindowHelper.WM_SYSCOMMAND, (IntPtr)WindowHelper.SC_SIZE_BOTTOMRIGHT, IntPtr.Zero);
+            };
+
+            #endregion
         }
     }
 }
---
+++ Azelea.WPF.Controls/WindowHelper.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace Azelea.WPF.Controls
+{
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct POINT
+    {
+        public int X;
+        public int Y;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct MINMAXINFO
+    {
+        public POINT Reserved;
+        public POINT MaxSize;
+        public POINT MaxPosition;
+        public POINT MinTrackSize;
+        public POINT MaxTrackSize;
+    };
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+    internal class MONITORINFO
+    {
+        public int Size = Marshal.SizeOf(typeof(MONITORINFO));
+        public RECT Monitor;
+        public RECT Work;
+        public int Flags;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 0)]
+    internal struct RECT
+    {
+        public int Left;
+        public int Top;
+        public int Right;
+        public int Bottom;
+    }
+
+    public static class WindowHelper
+    {
+        public const int MONITOR_DEFAULTTONEAREST = 0x00000002;
+        public const int WM_SYSCOMMAND = 0x0112;
+        public const int WM_GETMINMAXINFO = 0x0024;
+        public const int SC_MOVEHEADER = 0xF012;
+        public const int SC_CLOSE = 61536;
+        public const int SC_SIZE_LEFT = 0xF001;
+        public const int SC_SIZE_RIGHT = 0xF002;
+        public const int SC_SIZE_TOP = 0xF003;
+        public const int SC_SIZE_TOPLEFT = 0xF004;
+        public const int SC_SIZE_TOPRIGHT = 0xF005;
+        public const int SC_SIZE_BOTTOM = 0xF006;
+        public const int SC_SIZE_BOTTOMLEFT = 0xF007;
+        public const int SC_SIZE_BOTTOMRIGHT = 0xF008;
+
+        public static IntPtr Handle = IntPtr.Zero;
+
+        public static void RepairWindow(Window window)
+        {
+            window.SourceInitialized += delegate
+            {
+                Handle = (new WindowInteropHelper(window)).Handle;
+                HwndSource source = HwndSource.FromHwnd(Handle);
+                if (source != null)
+                {
+                    source.AddHook(WindowHelper.WindowProc);
+                }
+            };
+        }
+
+        private static IntPtr WindowProc(IntPtr hWnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            switch (msg)
+            {
+                case WM_GETMINMAXINFO:
+                    WmGetMinMaxInfo(hWnd, lParam);
+                    handled = true;
+                    break;
+            }
+
+            return IntPtr.Zero;
+        }
+
+        private static void WmGetMinMaxInfo(IntPtr hWnd, IntPtr lParam)
+        {
+            var mmi = (MINMAXINFO)Marshal.PtrToStructure(lParam, typeof(MINMAXINFO));
+            IntPtr pMonitor = MonitorFromWindow(hWnd, MONITOR_DEFAULTTONEAREST);
+            if (pMonitor != IntPtr.Zero)
+            {
+                var mi = new MONITORINFO();
+                GetMonitorInfo(pMonitor, mi);
+                RECT rcWorkArea = mi.Work;
+                RECT rcMonitorArea = mi.Monitor;
+                mmi.MaxPosition.X = Math.Abs(rcWorkArea.Left - rcMonitorArea.Left);
+                mmi.MaxPosition.Y = Math.Abs(rcWorkArea.Top - rcMonitorArea.Top);
+                mmi.MaxSize.X = Math.Abs(rcWorkArea.Right - rcWorkArea.Left);
+                mmi.MaxSize.Y = Math.Abs(rcWorkArea.Bottom - rcWorkArea.Top);
+            }
+
+            Marshal.StructureToPtr(mmi, lParam, true);
+        }
+
+        [DllImport("user32")]
+        internal static extern IntPtr SendMessage(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam);
+        [DllImport("user32")]
+        internal static extern bool GetMonitorInfo(IntPtr hMonitor, MONITORINFO lpmi);
+        [DllImport("user32")]
+        internal static extern IntPtr MonitorFromWindow(IntPtr handle, int flags);
+        [DllImport("user32")]
+        internal static extern bool GetCursorPos(ref POINT point);
+    }
+}
